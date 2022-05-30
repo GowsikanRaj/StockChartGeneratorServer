9528a2465d11ff3aab6f4941b5e715bfce515bbe
GowsikanRaj <gowsikan25@gmail.com>
2022-05-30 10:32:19 -0400

    Changed URL for getting watchlist data

--- index.js
+++ index.js
@@ -21,7 +21,7 @@ mongoose
   )
   .catch((err) => console.log(err));
 
-app.get("/getWatchlist", (req, res) => {
+app.get("/", (req, res) => {
   watchlist
     .find()
     .then((result) => res.send(result))
